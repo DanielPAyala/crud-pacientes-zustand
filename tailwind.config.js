34b804d84934dffc3a73a52dd2982518d2c001d0
Carlos Daniel <carlosporrasayala@gmail.com>
2024-10-16 08:24:27 -0500

    Install react-hook-form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },

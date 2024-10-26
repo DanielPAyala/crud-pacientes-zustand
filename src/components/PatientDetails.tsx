667dee1cae56d34f6775a65ff47798e23089bbe0
Carlos Daniel <carlosporrasayala@gmail.com>
2024-10-26 06:09:19 -0500

    Incorporate Redux DevTools to inspect the Zustand state

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -19,10 +19,11 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label='Fecha Alta' data={patient.date.toString()} />
       <PatientDetailItem label='Síntomas' data={patient.symptoms} />
 
-      <div className='flex justify-between gap-3 mt-10'>
+      <div className='flex flex-col lg:flex-row gap-3 justify-between gap-3 mt-10'>
         <button
           className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'
           type='button'
+          onClick={() => getPatientById(patient.id)}
         >
           Editar
         </button>
